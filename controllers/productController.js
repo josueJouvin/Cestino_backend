b7c71c50567026eff670abec0191a3df3542abae
josue jouvin <josuejouvin27@gmail.com>
2023-08-24 22:04:31 -0500

    manejo de errores, validacion de req

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,16 @@
 import Product from "../models/Product.js";
 
 const addProduct = async (req, res) => {
+  const {name, products} = req.body
+  if(name.trim() === ""){
+    const error = new Error("Existen campos vacios");
+    return res.status(400).json({ msg: error.message });
+  }
+  if(!Array.isArray(products) || products.length === 0){
+    const error = new Error("Se requiere al menos 1 producto");
+    return res.status(400).json({ msg: error.message });
+  }
+  
   const product = new Product(req.body);
   product.seller = req.seller._id;
 
@@ -8,8 +18,7 @@ const addProduct = async (req, res) => {
     const savedProduct = await product.save();
     res.json(savedProduct);
   } catch (error) {
-    res.status(500).json({ error: "Este producto ya ha sido registrado"});
-
+    res.status(500).json({ msg: "Esta Canasta ya Existe"});
   }
 };
 
@@ -40,6 +49,15 @@ const updateProduct = async (req, res) => {
   const { id } = req.params;
   const {name, subtotal, profit, total, percentage, products} = req.body
 
+  if(name.trim() === ""){
+    const error = new Error("Existen campos vacios");
+    return res.status(400).json({ msg: error.message });
+  }
+  if(!Array.isArray(products) || products.length === 0){
+    const error = new Error("Se requiere al menos 1 producto");
+    return res.status(400).json({ msg: error.message });
+  }
+  
   if (id.length !== 24) {
     return res.status(403).json({ msg: "No Encontrado" });
   }
@@ -57,7 +75,6 @@ const updateProduct = async (req, res) => {
   product.image = req.body.image || product.image;
   product.name = req.body.name || product.name;
 
-  // Mapeo de IDs de productos existentes
   if (products && Array.isArray(products)) {
     // Actualizar productos internos (si es necesario)
     product.products = products.map(updatedProduct => {
@@ -71,8 +88,6 @@ const updateProduct = async (req, res) => {
       return updatedProduct;
     });
   }
-
-
   product.subtotal = subtotal || product.subtotal;
   product.percentage = percentage || product.percentage
   product.profit = profit || product.profit;
@@ -82,8 +97,7 @@ const updateProduct = async (req, res) => {
     const updatedProduct = await product.save();
     res.json(updatedProduct);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Error interno del servidor" });
+    res.status(500).json({ msg: "Esta Canasta ya Existe"});
   }
 }; 
 
@@ -106,7 +120,7 @@ const deleteProduct = async (req, res) => {
 
   try {
     await product.deleteOne();
-    res.json({ msg: "Producto eliminado" });
+    res.json({ msg: "Canasta eliminada" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error al eliminar el producto" });
