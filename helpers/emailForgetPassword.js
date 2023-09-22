04059ec63f0a3ac901d9e7dea68e6c7158bc6993
josue jouvin <josuejouvin27@gmail.com>
2023-09-21 23:23:37 -0500

    Utilizando Resend para el envio real de emails

--- helpers/emailForgetPassword.js
+++ helpers/emailForgetPassword.js
@@ -1,6 +1,9 @@
 import nodemailer from "nodemailer"
+import { Resend } from 'resend';
+
 
 const emailForgetPassword = async ({email, name, token}) =>{
+  /*
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -8,8 +11,8 @@ const emailForgetPassword = async ({email, name, token}) =>{
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
         }
-      });
-
+      }); 
+      
       const info = await transport.sendMail({
         from: "Cestino - Administra tus productos",
         to: email,
@@ -21,8 +24,29 @@ const emailForgetPassword = async ({email, name, token}) =>{
                <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
         `
       })
-
       console.log("enviado: %s", info.messageId)
+      
+      */
+
+      const resend = new Resend(process.env.EMAIL_API_KEY);
+
+      try {
+        const data = await resend.emails.send({
+          from: 'Cestino - Administra tus productos <cestino@cestino.com>',
+          to: email,
+          subject: 'Reestablece tu Password',
+          text: "Reestablece tu Password",
+          html: `<p>Hola: ${name}, has solicitado reestablecer tu password.</p>
+              <p>Ingresa al siguiente enlace para generar un nuevo password: <a href="${process.env.FRONTEND_URL}/auth/olvide-password/${token}">Reestablece tu password</a></p>
+        
+              <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+              `
+        });
+    
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
 }
 
 export default emailForgetPassword
--- helpers/emailRegister.js
+++ helpers/emailRegister.js
@@ -1,7 +1,8 @@
 import nodemailer from "nodemailer"
+import { Resend } from 'resend';
 
 const emailRegister = async ({email, name, token}) =>{
-    const transport = nodemailer.createTransport({
+    /*const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         auth: {
@@ -22,7 +23,29 @@ const emailRegister = async ({email, name, token}) =>{
         `
       })
 
-      console.log("enviado: %s", info.messageId)
+      console.log("enviado: %s", info.messageId)*/
+
+
+      const resend = new Resend(process.env.EMAIL_API_KEY);
+
+      try {
+        const data = await resend.emails.send({
+          from: 'Cestino - Administra tus productos <cestino@cestino.com>',
+          to: email,
+          subject: 'Comprueba tu cuenta en Cestino',
+          text: "Comprueba tu cuenta en Cestino",
+          html: `<p>Hola: ${name}, comprueba tu cuenta en Cestino.</p>
+                <p>Tu cuenta ya esta lista, solo debes comprobarla en el siguente enlace: <a href="${process.env.FRONTEND_URL}/auth/confirmar/${token}">Comprobar Cuenta</a></p>
+          
+                <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+                `
+        });
+    
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+
 }
 
 export default emailRegister
